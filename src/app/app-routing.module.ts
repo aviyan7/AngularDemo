6227ed759109ecffd93d9e67008ebbbb791ce7ff
aviyan7 <aviyandahal47@gmail.com>
2022-02-17 17:42:15 +0545

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent,
+  },
+  {
+    path:'another-page',
+    component:HomeComponent,
+  },
+  {
+    path:'home/:id',
+    component:HomeComponent,
+  },
+  {
+    path:'detail/:id/class/:classId',
+    component:DetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

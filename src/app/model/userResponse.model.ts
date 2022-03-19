fd2de894a5297d0dd1f08e7d9479e6a5517d46cd
aviyan7 <aviyandahal47@gmail.com>
2022-03-19 11:23:24 +0545

    Basic

--- src/app/model/userResponse.model.ts
+++ src/app/model/userResponse.model.ts
@@ -5,10 +5,19 @@ export class UserResponseModel {
     dob: string | undefined;
     address: string | undefined;
     contacts: Array<Contacts> = [];
+    contactDtos: Array<Contacts> = [];
 }
 
 export class Contacts {
  mobileNumber: string | undefined;
  email: string | undefined;
- id: string | undefined;
+ id: number | undefined;
+ userId: number|undefined;
+ user: User = new User()
+
+
+}
+
+export class User{
+    id: number|undefined;
 }
